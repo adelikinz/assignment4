059d2e6462e47232881207c50b1e53a4b13a67dd
Nasra Diini <nasra_diini@hotmail.com>
2024-04-12 13:06:28 +0100

    added one end-point, gitignore file, app and main.py files

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-from db_utils import db_pethaven_overview
+from flask import Flask, jsonify
+
 
 
 app = Flask(__name__)
@@ -10,12 +10,7 @@ def get_all_pets():
     pets = db_pethaven_overview()
     return jsonify(pets)
 
-    # API endpoint to adopt a pet
-@app.route('/adopt_pet', methods=['POST'])
-def adopt_pet():
-    data = request.json
-    pet_id = data.get('pet_id')
-    customer_name = data.get('customer_name')
+
 
 
 if __name__ == '__main__':
--- api/config.py
+++ api/config.py
@@ -1,3 +1,3 @@
 HOST="127.0.0.1"
 USER="root"
-PASSWORD="pethavenpassword"
+PASSWORD="password123"  #add your own mysql password in here
