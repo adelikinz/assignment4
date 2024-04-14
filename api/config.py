ec5524edc765c756815fb1f1e9fe303e3d567118
Nasra Diini <nasra_diini@hotmail.com>
2024-04-14 14:56:28 +0100

    added my endpoint, function and comments

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
-HOST = "localhost" # change host if needed here
+HOST = "127.0.0.1" # change host if needed here
 USER = "root" # change user if needed here
-PASSWORD = "***" # update to your password here
+PASSWORD = "###" # update to your password here
 
