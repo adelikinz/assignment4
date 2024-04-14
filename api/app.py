ae5e5280bb084a7046fc45dc5167911a2c369c95
Cynth2208 <xc-darkxz@hotmail.co.uk>
2024-04-14 01:26:50 +0100

    2 endpoints created for renting and adopting pets. And accompanying functions

--- api/app.py
+++ api/app.py
@@ -1,40 +1,62 @@
 from flask import Flask, request, jsonify
-from db_utils import db_call_without_values, create_customer_db, db_call_with_values_without_return, db_call_with_values, find_customer_db
+from db_utils import db_call_without_values, create_customer_db, db_call_with_values_without_return, db_call_with_values, find_customer_db, add_booking_to_db, cancel_bookings, update_pet_availability
 app = Flask(__name__)
 
 @app.route('/pets', methods=[])
 def get_available_pets():
-# endpoint to retrieve pets that are available
+    # endpoint to retrieve pets that are available
+    pass
 
 
 @app.route('/dates', methods=[])
 def get_available_dates():
-# endpoint to retrieve available dates
+    # endpoint to retrieve available dates
+    pass
 
 
 @app.route('/timeslots', methods=[])
 def get_available_timeslots():
-# endpoint to retrieve available timeslots for the available pets
-
+    # endpoint to retrieve available timeslots for the available pets
+    pass
 
 @app.route('/create_customer', methods=[])
 def create_customer():
-# endpoint to create a customer in the customer table as a user
+    # endpoint to create a customer in the customer table as a user
+    pass
 
 
 @app.route('/find_customer', methods=[])
 def find_customer():
-# endpoint to find an existing customer as a user
+    # endpoint to find an existing customer as a user
+    pass
 
-
-@app.route('/rent', methods=[])
+@app.route('/rent', methods=['POST'])
 def rent_pet():
-# endpoint to fill in the booking table for renting
+    # endpoint to fill in the booking table for renting
+    renting_data = request.json
+    pet_id = renting_data.get('pet_id')
+    customer_id = renting_data.get('customer_id')
+    timeslot = renting_data.get('timeslot')
+    booking_date = renting_data.get('booking_date')
+
+    new_booking = add_booking_to_db(pet_id, customer_id, timeslot, booking_date)
+
+    return jsonify(new_booking), 201
 
 
-@app.route('/adopt', methods=[])
-def adopt_pet():
 # endpoint for adopting where it cancels any existing bookings and removes pet from availability
+@app.route('/adopt', methods=['POST'])
+def adopt_pet(data=None):
+    adoption_data = request.json
+    pet_id = data.get('pet_id')
+
+    # Cancel any existing bookings for the pet due to pending adoption
+    cancel_bookings(pet_id)
+
+    # Update pet availability to not available for rent due to adoption
+    update_pet_availability(pet_id)
+
+    return jsonify({"message": "Successful adoption"}), 200
 
 
 if __name__ == '__main__':
--- api/db_utils.py
+++ api/db_utils.py
@@ -7,26 +7,119 @@ class DbConnectionError(Exception):
 # every function should be in a try/catch statement as we are connecting to database here
 
 def _connect_to_db(db_name):
+    # establish connection to database using config file
+    db_connection = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=db_name
+    )
+    return db_connection
 # establish connection to database using config file
 
 
-def db_call_without_values(query):
-# general database call that executes query but doesn't return anything.
-
-
 def find_customer_db(query, values):
-# database call that searches for customer id by existing customer name and email.
-
+    # database call that searches for customer id by existing customer name and email.
+    pass
 
 def create_customer_db(query):
-# creates customer in the database with the inputted customer name and email should also return customer id to be used elsewhere
+    # creates customer in the database with the user-input customer name and email, this also returns customer id to be used elsewhere.
+    pass
 
+def db_call_without_values(query):
+    # general database call that executes query but doesn't return anything.
+    pass
 
 def db_call_with_values_without_return(query, values):
-# general db call that takes values and does not return anything
+    # general db call that takes values and does not return anything
+    pass
 
 
 def db_call_with_values(query, values):
-# general db call that takes query with values and returns results
+    # general db call that takes query with values and returns results
+    pass
 
 
+# Add booking to the SQL database
+def add_booking_to_db(pet_id, customer_id, timeslot, booking_date):
+    try:
+        # Connect to the database
+        db_name = 'pethaven_db'
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
+        print("Connected to Database")
+
+        # the SQL query for updating the pet bookings
+        query = """
+        UPDATE pet_bookings
+        SET pet_id = {}, timeslot_{} = {}, booking_id_{} = {}
+        WHERE bookingDate = '{}';
+        """.format(pet_id, timeslot, customer_id, timeslot, customer_id, booking_date)
+
+        # Executes the query and then commits
+        cur.execute(query)
+        db_connection.commit()
+
+        cur.close()
+
+        # returns the booking details
+        return {'pet_id': pet_id, 'customer_id': customer_id, 'timeslot': timeslot, 'booking_date': booking_date}
+
+    except Exception as e:
+        raise DbConnectionError("Failed to read data in Database") from e
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
+
+
+def cancel_bookings(pet_id):
+    try:
+        db_name = 'pethaven_db'
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
+
+        # Cancel existing bookings for the pet
+        query = """
+        UPDATE pet_bookings
+        SET booking_id_9_11 = NULL, booking_id_13_15 = NULL, booking_id_17_19 = NULL
+        WHERE pet_id = %s
+        """
+        cur.execute(query, (pet_id,))
+        db_connection.commit()
+
+        cur.close()
+
+    except mysql.connector.Error as e:
+        raise DbConnectionError("Failed cancellation of existing bookings") from e
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
+
+def update_pet_availability(pet_id):
+    try:
+        db_name = 'pethaven_db'
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
+
+        # Update pet availability to not available for rent
+        query = """
+        UPDATE pets
+        SET available_for_rent = 0
+        WHERE pet_id = %s
+        """
+        cur.execute(query, (pet_id,))
+        db_connection.commit()
+
+        cur.close()
+
+    except mysql.connector.Error as e:
+        raise DbConnectionError("Update failure for pet availability") from e
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
